0449fc793221c89c3a6554eda23c45d4f84d9069
Marlon Frade <marlonmelo98@hotmail.com>
2022-11-14 16:01:41 -0300

    [layout-2] - working on Home Page Components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,19 @@
 import Hero from "../components/Hero";
+import Feature from "../components/Feature";
+import Content from "../components/Content";
+import Faq from "../components/Faq";
+import Statistic from "../components/Statistic";
+import Cta from "../components/Cta";
 
 export default function Home() {
   return (
-    <div className=" bg-white dark:bg-slate-800 py-6 sm:py-12">
+    <div className="bg-white dark:bg-black py-6 sm:py-12">
       <Hero />
+      <Feature />
+      <Content />
+      <Statistic />
+      <Faq />
+      <Cta />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import LogoLightMode from "../images/logo-monkey-light-mode.svg";
 import LogoDarkMode from "../images/logo-monkey-dark-mode.svg";
-import ThemeIcon from "../images/dark-mode-icon.svg";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -48,33 +47,6 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-        <svg
-          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
       <div className="px-6 pt-6 lg:px-8">
         <div>
           <nav
@@ -83,7 +55,7 @@ export default function Navbar() {
           >
             <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
               <Link to="/">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Monkey Branch</span>
                 {theme === "dark" ? (
                   <img src={LogoDarkMode} alt="logo dark mode" />
                 ) : (
@@ -94,7 +66,7 @@ export default function Navbar() {
             <div className="flex lg:hidden">
               <button
                 type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-red-500"
                 onClick={() => setMobileMenuOpen(true)}
               >
                 <span className="sr-only">Open main menu</span>
@@ -106,7 +78,7 @@ export default function Navbar() {
                 <Link
                   to={item.href}
                   key={item.name}
-                  className="font-semibold text-gray-900 dark:  hover:text-gray-900"
+                  className="font-semibold text-gray-900 dark:text-white hover:dark:text-gray-300  hover:text-gray-700"
                 >
                   {item.name}
                 </Link>
@@ -129,8 +101,8 @@ export default function Navbar() {
                     fill={theme === "dark" ? "white" : "black"}
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
                     fill={theme === "dark" ? "white" : "black"}
                   />
@@ -138,7 +110,7 @@ export default function Navbar() {
               </div>
               <Link
                 to="/login"
-                className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                className="inline-block rounded-lg bg-red-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-red-600 hover:bg-red-500 hover:ring-red-500"
               >
                 Log in
               </Link>
@@ -147,23 +119,23 @@ export default function Navbar() {
           <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <Dialog.Panel
               focus="true"
-              className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
+              className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden dark:bg-black text-white"
             >
               <div className="flex h-9 items-center justify-between">
                 <div className="flex">
                   <Link to="/" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
-                    <img
-                      className="h-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
+                    <span className="sr-only">MonkeyBranch</span>
+                    {theme === "dark" ? (
+                      <img src={LogoDarkMode} alt="logo dark mode" />
+                    ) : (
+                      <img src={LogoLightMode} alt="Logo light mode" />
+                    )}
                   </Link>
                 </div>
                 <div className="flex">
                   <button
                     type="button"
-                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-red-500 hover:text-red-700"
                     onClick={() => setMobileMenuOpen(false)}
                   >
                     <span className="sr-only">Close menu</span>
@@ -172,21 +144,21 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="-my-6 divide-y divide-gray-900/10 dark:divide-gray-100/20">
                   <div className="space-y-2 py-6">
                     {navigation.map((item) => (
                       <Link
                         key={item.name}
                         to={item.href}
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 dark:text-white hover:dark:text-gray-300  hover:text-gray-700"
                       >
                         {item.name}
                       </Link>
                     ))}
                   </div>
-                  <div className="py-6">
+                  <div className="py-6 flex items-center flex-start">
                     <div
-                      className="flex items-center h-10 w-16 cursor-pointer"
+                      className="flex items-center h-8 w-10 cursor-pointer mb-3"
                       onClick={handleThemeSwitch}
                     >
                       <svg
@@ -201,8 +173,8 @@ export default function Navbar() {
                           fill={theme === "dark" ? "white" : "black"}
                         />
                         <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
+                          fillRule="evenodd"
+                          clipRule="evenodd"
                           d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
                           fill={theme === "dark" ? "white" : "black"}
                         />
@@ -210,7 +182,7 @@ export default function Navbar() {
                     </div>
                     <Link
                       to="/login"
-                      className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                      className="inline-block rounded-lg bg-red-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-red-600 hover:bg-red-500 hover:ring-red-500"
                     >
                       Log in
                     </Link>
